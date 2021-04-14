553cfe47019517be4457f70262a641e0c3beb9a0
p-alex <alexey9021@gmail.com>
2021-04-14 21:01:23 +0300

    Added Footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import Jobs from "./containers/Jobs/Jobs";
 import JobFilters from "./components/JobFilters/JobFilters";
+import Footer from "./components/Footer/Footer";
 import { useSelector } from "react-redux";
 function App() {
   const filters = useSelector((state) => state.filters);
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <JobFilters />
       <Jobs />
+      <Footer />
     </>
   );
 }
